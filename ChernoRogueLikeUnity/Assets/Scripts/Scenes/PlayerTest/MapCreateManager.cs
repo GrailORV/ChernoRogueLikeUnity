2ae5963dde32523910593dd40eef6c1d785afecd
ueno <saku39sky@gmail.com>
2018-10-13 23:50:29 +0900

    小川の力を継承

---
+++ ChernoRogueLikeUnity/Assets/Scripts/Scenes/PlayerTest/MapCreateManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreateManager : MonoBehaviour {
+
+    [SerializeField] TextAsset csvFile;
+
+    private List<string[]> csvDatas = new List<string[]>();
+    private int csvHeight = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
---
+++ ChernoRogueLikeUnity/Assets/Scripts/Scenes/PlayerTest/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// プレイヤーの移動
+    /// </summary>
+    void PlayerKeyMove()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
