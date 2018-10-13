37baf7d689511098bae813e81f01374b6941b761
DESKTOP-VI7U7ON\gesky <aaa>
2018-10-14 01:31:19 +0900

    いくょ

--- ChernoRogueLikeUnity/Assets/Scripts/Scenes/PlayerTest/MapCreateManager.cs
+++ ChernoRogueLikeUnity/Assets/Scripts/Scenes/PlayerTest/MapCreateManager.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MapCreateManager : MonoBehaviour {
 
     [SerializeField] TextAsset csvFile;
 
-    private List<string[]> csvDatas = new List<string[]>();
-    private int csvHeight = 0;
+    string str = "";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private int Vertical = 9;
+    private int Horizontal = 19;
+
+    enum MAPOBJ
+    {
+        plane,
+        wall,
+        player,
+        enemy,
+        item,
+        exit
+    }
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        StringReader reader = new StringReader(csvFile.text);
+
+        while (reader.Peek() > -1)
+        {
+            string line = reader.ReadLine();
+            str = str + line;
+        }
+
+        str = str + ",";
+    }
 }
